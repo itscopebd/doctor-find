10247943209c74e37001f5541e692ac038a71b18
itscopebd <itscopebd@gmail.com>
2023-08-19 13:59:42 +0600

    header logo nav bar added

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,11 +1,66 @@
 import React from 'react';
-
+import logo from "./../../assets/images/logo.png";
+import userImage from "./../../assets/images/avatar-icon.png"
 const Header = () => {
+
+    const navBar = <>
+
+        <li><a>Home</a></li>
+        <li> <a>Find a Doctor</a></li>
+        <li><a>Services</a></li>
+        <li><a>Contact</a></li>
+
+    </>
+
+
     return (
-        <div>
-            <h3>Header</h3>
+        <div className='header'>
+            <div className='container mx-auto'>
+                <div className="navbar">
+                    <div className="navbar-start">
+                        <div className="dropdown">
+                            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                            </label>
+                            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                                {navBar}
+                            </ul>
+                        </div>
+                        <a className="btn btn-ghost normal-case text-xl">
+                            <img src={logo} alt="" />
+                        </a>
+                    </div>
+                    <div className="navbar-center hidden lg:flex">
+                        <ul className="menu menu-horizontal px-1">
+                            {navBar}
+                        </ul>
+                    </div>
+                    <div className="navbar-end">
+                        <div className="dropdown dropdown-end">
+                            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                                <div className="w-10 rounded-full">
+                                    <img src={userImage} />
+                                </div>
+                            </label>
+                            <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+                                <li>
+                                    <a className="justify-between">
+                                        Profile
+                                        <span className="badge">New</span>
+                                    </a>
+                                </li>
+                                <li><a>Settings</a></li>
+                                <li><a>Logout</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
+
+
+
 
 export default Header;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -17,5 +17,5 @@ export default {
       boxShadow:"rgba(17,12,46,0.15) 0px 48px 100px 0px;"
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 }
